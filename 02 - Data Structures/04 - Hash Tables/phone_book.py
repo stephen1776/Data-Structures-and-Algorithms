6b6cef6ff861d17e9138de55919c0530a3792214
Stephen Blatti <stephen.p.blatti@gmail.com>
2017-11-07 19:04:25 -0600

    Update

--- 02 - Data Structures/04 - Hash Tables/phone_book.py
+++ 02 - Data Structures/04 - Hash Tables/phone_book.py
@@ -42,34 +42,6 @@ def process_queries(queries):
 
 
 
-# def process_queries(queries):
-#     result = []
-#     # Keep list of all existing (i.e. not deleted yet) contacts.
-#     contacts = []
-#     for cur_query in queries:
-#         if cur_query.type == 'add':
-#             # if we already have contact with such number,
-#             # we should rewrite contact's name
-#             for contact in contacts:
-#                 if contact.number == cur_query.number:
-#                     contact.name = cur_query.name
-#                     break
-#             else: # otherwise, just add it
-#                 contacts.append(cur_query)
-#         elif cur_query.type == 'del':
-#             for j in range(len(contacts)):
-#                 if contacts[j].number == cur_query.number:
-#                     contacts.pop(j)
-#                     break
-#         else:
-#             response = 'not found'
-#             for contact in contacts:
-#                 if contact.number == cur_query.number:
-#                     response = contact.name
-#                     break
-#             result.append(response)
-#     return result
-
 if __name__ == '__main__':
     write_responses(process_queries(read_queries()))
 
